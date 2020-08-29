d34511c2c29088623457c0ca32017478c8f04967
Christian Ortega <christianmortega7@gmail.com>
2020-08-29 00:08:15 -0400

    Implement to_s function

--- linked_list.rb
+++ linked_list.rb
@@ -2,7 +2,7 @@ Node = Struct.new(:value, :next_node)
 
 class LinkedList
 
-  attr_accessor :root
+  attr_accessor :root, :tail, :size
 
   def initialize
     @root = nil
@@ -31,4 +31,21 @@ class LinkedList
     end
     self.size += 1
   end
+
+  def to_s
+    pointer = root
+    list_as_string = ""
+    until pointer == nil do
+      list_as_string += "( #{pointer.value} ) -> "
+      pointer = pointer.next_node
+    end
+    list_as_string += "nil"
+  end
 end
+
+list = LinkedList.new
+5.times do |i|
+  list.append(i)
+  list.prepend(i)
+end
+p list.to_s
