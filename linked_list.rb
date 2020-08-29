ac11fa98a6f76a8815c9f0ba560b64fafedb643c
Christian Ortega <christianmortega7@gmail.com>
2020-08-28 23:52:31 -0400

    Implement append function

--- linked_list.rb
+++ linked_list.rb
@@ -2,5 +2,23 @@ Node = Struct.new(:value, :next_node)
 
 class LinkedList
 
-end
+  attr_accessor :root
 
+  def initialize
+    @root = nil
+    @tail = nil
+    @size = 0
+  end
+
+  def append(value)
+    if root == nil
+      self.root = Node.new(value)
+      self.tail = root
+      self.size += 1
+    else
+      tail.next_node = Node.new(value)
+      self.tail = tail.next_node
+      self.size += 1
+    end
+  end
+end
